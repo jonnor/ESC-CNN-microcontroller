b5e10b82d9d1b9fdeae92652265f380904fe6527
Jon Nordby <jononor@gmail.com>
2019-04-01 21:08:17 +0200

    Code for generating variations of SB-CNN

---
+++ microesc/variations.py
@@ -0,0 +1,107 @@
+
+import uuid
+import os.path
+
+import sklearn
+import pandas
+
+import models, stm32convert
+
+def sbcnn_generator(n_iter=10, random_state=1):
+
+    from sklearn.model_selection import ParameterSampler
+
+    params = dict(
+        kernel_t = range(1, 7),
+        kernel_f = range(1, 7),
+        pool_t = range(2, 5),
+        pool_f = range(2, 5),
+        kernels_start = range(16, 64),
+        fully_connected = range(16, 128),
+    )
+
+    sampler = ParameterSampler(params, n_iter=n_iter, random_state=random_state)
+    
+    out_models = []
+    out_total_params = []
+    for p in sampler:
+        s = {
+            'model': 'sbcnn',
+            'frames': 31,
+            'n_mels': 60,
+            'samplerate': 22050,
+        }
+
+        pool = p['pool_t'], p['pool_f']
+        kernel = p['kernel_t'], p['kernel_f']
+        for k, v in p.items():
+            s[k] = v
+        s['pool'] = pool
+        s['kernel'] = kernel
+
+        print('s', s)
+
+        try:
+            m = models.build(s)
+        except ValueError as e:
+            yield e
+
+        yield m, p
+
+
+def generate_models():
+
+    gen = sbcnn_generator()
+    data = {
+        'model_path': [],
+        'gen_path': [],
+        'id': [],
+    }
+    for out in gen:
+        try:
+            model, params = out
+        except TypeError:
+            print('Error:', out)
+
+        # Store parameters
+        for k, v in params.items():
+            if data.get(k) is None:
+                data[k] = []
+            data[k].append(v)
+
+        model_id = str(uuid.uuid4())
+        out_dir = os.path.join('scan', model_id)
+        os.makedirs(out_dir)
+
+        model_path = os.path.join(out_dir, 'model.orig.hdf5')   
+        out_path = os.path.join(out_dir, 'gen')     
+   
+        # Store model
+        print('pp', model_path)
+        model.save(model_path)
+        stats = stm32convert.generatecode(model_path, out_path,
+                                  name='network', model_type='keras', compression=None)
+
+        # Store model info
+        data['model_path'].append(model_path)
+        data['gen_path'].append(out_path)     
+        data['id'].append(model_id)    
+
+        print('s', stats.values(), model.count_params())
+
+        for k, v in stats.items():
+            if data.get(k) is None:
+                data[k] = []
+            data[k].append(v)
+
+    df = pandas.DataFrame(data)
+    return df
+
+def main():
+
+    df = generate_models()
+    df.to_csv('scan.csv')
+
+
+if __name__ == '__main__':
+    main()
