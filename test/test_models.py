349b088938e2cecd13c050e82b14c9b438785378
Jon Nordby <jononor@gmail.com>
2019-04-06 16:38:04 +0200

    test: Also convert models
    Take a very long time and depends on X-CUBE-AI

--- test/test_models.py
+++ test/test_models.py
@@ -1,9 +1,14 @@
 
+import os.path
+
 import pytest
 import keras
 
-from microesc import models
+from microesc import models, stm32convert
 
+out_dir = os.path.join(os.path.dirname(__file__), 'out')
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 FAMILIES=list(models.families.keys())
 
@@ -19,5 +24,12 @@ def test_models_basic(family):
         s['kernel'] = (3, 3)
 
     m = models.build(s)
-    
     assert isinstance(m, keras.Model)
+
+    model_name = family+'.default'
+    model_path = os.path.join(out_dir, model_name+'.hdf5')
+    gen_path = os.path.join(out_dir, model_name)
+    m.save(model_path)
+    stats = stm32convert.generatecode(model_path, gen_path,
+                                name='network', model_type='keras', compression=None)
+
