5e24c76714dc9c63a335c734f12e55ef117f7ef0
Jon Nordby <jononor@gmail.com>
2019-04-06 23:40:20 +0200

    Make effnet fit
    Tops at 62% before heavy overfitting

--- microesc/models/dense.py
+++ microesc/models/dense.py
@@ -3,11 +3,7 @@ def build_model(bands=60, frames=41, channels=1, n_labels=10,
                 dropout=0.0, depth=7, block=2, growth=15, pooling='avg',
                 bottleneck=False, reduction=0.0, subsample=True):
     """
-    LD-CNN: A Lightweight Dilated Convolutional Neural Network for Environmental Sound Classification
-
-    http://web.pkusz.edu.cn/adsp/files/2015/10/ICPR_final.pdf 
-    had comparison with DenseNet, which was claimed to do well.
-    However, no details on the parameters used...
+    DenseNet
     """
 
     # https://github.com/keras-team/keras-contrib/blob/master/keras_contrib/applications/densenet.py
