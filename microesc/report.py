9a00f79f09ef15d65aab1e06d8337968d234105f
Jon Nordby <jononor@gmail.com>
2019-04-08 17:17:04 +0200

    Add initial results, boxplot

--- microesc/report.py
+++ microesc/report.py
@@ -3,8 +3,9 @@ import os.path
 import sys
 
 import numpy
-import seaborn as sns
+import seaborn
 import matplotlib.pyplot as plt     
+import pandas
 
 import common, urbansound8k
 #from . import common, urbansound8k
@@ -27,7 +28,7 @@ def plot_confusion(cm, classnames, normalize=False, percent=False):
         fmt = ".1f"
 
     fig, ax = plt.subplots(1, figsize=(10,8))
-    sns.heatmap(cm, annot=True, ax=ax, fmt=fmt);
+    seaborn.heatmap(cm, annot=True, ax=ax, fmt=fmt);
 
     ax.set_xlabel('Predicted labels')
     ax.set_ylabel('True labels')
@@ -98,17 +99,16 @@ def print_accuracies(accs, title):
     [ print("{:.3f}".format(v), end=',') for v in accs ]
     print('\n')
 
-def main():
 
-    args = parse(None)
 
-    cm = numpy.load(os.path.join(args.results_dir, args.run, '{}.confusion.npz'.format(1)))
+
+def stats():
+
     val, test = cm['val'], cm['test']
 
-
     classnames = urbansound8k.classnames
-    val_fig = plot_confusion(100*numpy.mean(val, axis=0), classnames, normalize=True)
-    test_fig = plot_confusion(100*numpy.mean(test, axis=0), classnames, normalize=True) 
+    val_fig = plot_confusion(numpy.mean(val, axis=0), classnames, percent=True)
+    test_fig = plot_confusion(numpy.mean(test, axis=0), classnames, percent=True) 
     val_fig.savefig('val.cm.png')
     test_fig.savefig('test.cm.png')
 
@@ -122,6 +122,57 @@ def main():
     print_accuracies(c_acc, 'class_acc')
 
     print('wrote')
+
+def get_accuracies(confusions):
+    accs = [ cm_accuracy(confusions[f]) for f in range(0, len(confusions)) ]
+    assert len(accs) == 9, len(accs) 
+    return pandas.Series(accs) 
+
+def plot_accuracy_comparison(experiments):
+    
+    acc = experiments.confusions_test.apply(get_accuracies).T
+    print(acc)
+
+    fig, ax = plt.subplots(1)
+    acc.boxplot(ax=ax)
+    fig.savefig('accs.png')
+    return fig
+
+
+def load_results(input_dir, suffix='.confusion.npz'):
+
+    files = os.listdir(input_dir)
+    files = [ f for f in files if f.endswith(suffix) ]
+    names = [ f.rstrip(suffix) for f in files ]
+
+    df = pandas.DataFrame({
+        'experiment': names,
+        'result_path': [ os.path.join(input_dir, f) for f in files ],
+    })
+
+    def load_results(row):
+        print('rr')
+        path = row['result_path']
+        results = numpy.load(path)
+        for k, v in results.items():
+            row['confusions_'+k] =v
+        return row
+
+
+    df = df.apply(load_results, axis=1)
+    return df
+
+
+def main():
+    args = parse(None)
+
+    # TODO: plot MACCS vs accuracy
+
+    input_dir = os.path.join(args.results_dir, args.run)
+    df = load_results(input_dir)
+    print('res\n', df)
+    plot_accuracy_comparison(df)
+
 
 if __name__ == '__main__':
     main()
